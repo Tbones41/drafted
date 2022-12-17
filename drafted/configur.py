e84e6bcabe2d2f096b3f1e42a8a339536949ffff
Tolulope Balogun <tbones4665@gmail.com>
2022-12-17 20:41:41 +0000

    Hid keys

---
+++ drafted/configur.py
@@ -0,0 +1,1 @@
+s_key = "django-insecure-2qpq&9j^^!oxktll+%27ow18%2t-w*2i(-49l1t74cw4-$(@+o"
--- drafted/settings.py
+++ drafted/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import configur
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2qpq&9j^^!oxktll+%27ow18%2t-w*2i(-49l1t74cw4-$(@+o"
+SECRET_KEY = configur.s_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
